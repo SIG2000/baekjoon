223926f0da1506eef432bf51c98fe6a5a70f751c
song <song2000yln@gmail.com>
2020-03-03 10:04:16 +0900

    finished

--- baekjoon1753.cpp
+++ baekjoon1753.cpp
@@ -5,7 +5,7 @@
 #include <iostream>	
 #include <queue>	
 #include <vector>	
-#include <map>
+#include <unordered_map>
 using namespace std;
 
 using Edge = struct Edge {
@@ -13,47 +13,38 @@ using Edge = struct Edge {
 	int destination;
 	int distance;
 
-	Edge& operator()(int dest, int dis) {
-
-		destination = dest;
-		distance = dis;
-
-		return *this;
-	}
-};
-
-inline istream& operator>>(istream& in, Edge& route) {
-
-	cin >> route.destination >> route.distance;
-	return in;
-}
-
-class Compare {
-public :
 	bool operator()(const Edge &a, const Edge &b) {
 		return a.distance > b.distance;
 	}
 };
 
-using p_queue = priority_queue<Edge, vector<Edge>, Compare>;
+inline istream& operator>>(istream& in, Edge& edge) {
 
-void minimumRoute(vector<int> &, multimap<int, Edge> &, int);
+	cin >> edge.destination >> edge.distance;
+	return in;
+}
+
+using p_queue = priority_queue<Edge, vector<Edge>, Edge>;
+
+void minimumRoute(vector<int> &, unordered_multimap<int, Edge> &, int);
 inline void printeRoute(vector<int> &);
 
 int main() {
 
 	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
 	int N, M, i, stand, origin; // stand = standard node
 	cin >> N >> M >> stand;
 
 	// 간선 입력
-	multimap<int, Edge> graph;
+	unordered_multimap<int, Edge> graph;
 	Edge edge;
-	for (i = 0; i < M; i++)	{
+	for (i = 0; i < M; i++) {
 
 		cin >> origin >> edge;
-		graph.insert(make_pair(origin, edge));
+		graph.insert({ origin, edge });
 	}
 
 	// 초기 거리 지정
@@ -69,17 +60,16 @@ int main() {
 	return 0;
 }
 
-void minimumRoute(vector<int> &distance, multimap<int, Edge> &graph, int stand) {
+void minimumRoute(vector<int> &distance, unordered_multimap<int, Edge> &graph, int stand) {
 
 	p_queue edgeQue;
 	const int size = static_cast<int>(distance.size());
 
-	Edge edge;
-	edgeQue.push(edge(stand, 0)); // 시작 위치
+	edgeQue.push({ stand, 0 }); // 시작 위치
 
 	int i;
-	for (i = 0; i < stand; i++) edgeQue.push(edge(i, INT32_MAX));
-	for (++i; i < size; i++) edgeQue.push(edge(i, INT32_MAX));
+	for (i = 0; i < stand; i++) edgeQue.push({ i, INT32_MAX });
+	for (++i; i < size; i++) edgeQue.push({ i, INT32_MAX });
 	// 나머지 위치에 infinity
 
 	vector<bool> visited(size, false);
@@ -92,7 +82,7 @@ void minimumRoute(vector<int> &distance, multimap<int, Edge> &graph, int stand) 
 		if (!visited[current_stand]) {
 			visited[current_stand] = true;
 			// ----------------------------------------------------------------------------------
-			// pair_iter = pair<multimap<int, Route>::iterator, multimap<int, Route>::iterator>
+			// pair_iter = pair<unordered_multimap<int, Route>::iterator, unordered_multimap<int, Route>::iterator>
 			auto pair_iter = graph.equal_range(current_stand);
 			for (auto iter = pair_iter.first; iter != pair_iter.second; iter++) {
 
@@ -104,9 +94,7 @@ void minimumRoute(vector<int> &distance, multimap<int, Edge> &graph, int stand) 
 				if (distance[dest] > temp) {
 					distance[dest] = temp;
 
-					edge = iter->second;
-					edge.distance = temp;
-					edgeQue.push(edge);
+					edgeQue.push({ dest, temp });
 				}
 				// 더 작은 비용이면 갱신
 			}
